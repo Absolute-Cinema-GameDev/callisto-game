cd7f3d97f2b2586fa3de43f7052803182b39449e
Mariano Gerardus Senduk <121662118+nano141004@users.noreply.github.com>
2025-05-16 17:16:21 +0700

    refactor(seaweed-bush): add animation

--- scenes/levels/seaweed_bush/seaweed_bush.gd
+++ scenes/levels/seaweed_bush/seaweed_bush.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
+@onready var anim = $Animate
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass  # Replace with function body.
+	anim.play("idle")
 
 
 func _on_area_detection_body_entered(body: Node2D) -> void:
