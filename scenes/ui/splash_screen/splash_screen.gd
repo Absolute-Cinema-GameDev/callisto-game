07842b047676a2791d2babc64e1712c64835491b
mma <111668501+mmalkindi@users.noreply.github.com>
2025-05-19 17:13:31 +0700

    feat: polish splash screen

--- scenes/ui/splash_screen/splash_screen.gd
+++ scenes/ui/splash_screen/splash_screen.gd
@@ -1,10 +1,15 @@
 extends Control
 
+const LOGO_FADE_IN_TIME := 1.0
+const LOGO_FADE_OUT_TIME := 1.5
+const SPLASH_SCREEN_HANG_TIME := 2.0
+const LOGO_SHOWN_COLOR := Color(1, 1, 1, 1)
+const LOGO_HIDDEN_COLOR := Color(1, 1, 1, 0)
+
 
 func splash_screen_done() -> void:
 	# TODO: change scene when done
 	print("A game by Absolute Cinema")
-	pass
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -15,8 +20,12 @@ func _unhandled_input(event: InputEvent) -> void:
 func _ready() -> void:
 	var teamlogo = $TeamLogo
 	var tween = create_tween()
-	tween.tween_property(teamlogo, "self_modulate", Color(1, 1, 1, 1), 1).from(Color(1, 1, 1, 0))
-	await get_tree().create_timer(3).timeout
+	(
+		tween
+		. tween_property(teamlogo, "self_modulate", Color(LOGO_SHOWN_COLOR), LOGO_FADE_IN_TIME)
+		. from(LOGO_HIDDEN_COLOR)
+	)
+	await get_tree().create_timer(SPLASH_SCREEN_HANG_TIME).timeout
 	var tween2 = create_tween()
-	tween2.tween_property(teamlogo, "self_modulate", Color(1, 1, 1, 0), 2)
+	tween2.tween_property(teamlogo, "self_modulate", Color(LOGO_HIDDEN_COLOR), LOGO_FADE_OUT_TIME)
 	splash_screen_done()
