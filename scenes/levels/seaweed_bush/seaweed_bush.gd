ddd6912571f4e3c06883e588ae3f255810e61bee
Mariano Gerardus Senduk <121662118+nano141004@users.noreply.github.com>
2025-05-22 00:35:16 +0700

    ambush polished (2), seaweed bush polished (2)

--- scenes/levels/seaweed_bush/seaweed_bush.gd
+++ scenes/levels/seaweed_bush/seaweed_bush.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
 @onready var anim = $Animate
+@onready var area_detection = $AreaDetection
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim.play("idle")
 
+func _process(_delta: float) -> void:
+	for body in area_detection.get_overlapping_bodies():
+		if body is Player:
+			body.set_is_hidden_from_enemies(true)
 
 func _on_area_detection_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.set_is_hidden_from_enemies(true)
+	pass
+# 	if body is Player:
+# 		body.set_is_hidden_from_enemies(true)
 
 
 func _on_area_detection_body_exited(body: Node2D) -> void:
